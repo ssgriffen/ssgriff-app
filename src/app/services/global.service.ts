df8cebf31f0fc6bb1bc2468abdb31632c096ad43
Sam Griffen <samsgriffen@gmail.com>
2017-11-07 14:46:14 -0700

    updated global service for easier backend switching

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -6,19 +6,22 @@ export class GlobalService {
   BASE_IMG_URL: string = "https://s3-us-west-1.amazonaws.com/sam-app-bucket"
   
   constructor() {
-    this.BASE_URL = this.setUrl();
-    // this.BASE_URL = "http://localhost:8000"
+    //backends provide identical support made 2 for practice
+    // this.BASE_URL = this.setUrl("flask");
+    this.BASE_URL = this.setUrl("node");
    }
 
-   setUrl(): string {
+   setUrl(backend: string): string {
 
     if(isDevMode()){
-      return "http://127.0.0.1:5000";
-      // return "http://localhost:8000";
-      // return "https://api-samgriffen-v2.herokuapp.com/";
+      if(backend === "flask")  return "http://127.0.0.1:5000";
+       
+      return "http://localhost:8000";
     }
-    return "https://api-samgriffen.herokuapp.com";
-    // return "https://api-samgriffen-v2.herokuapp.com/";
+
+    if(backend === "flask") return "https://api-samgriffen.herokuapp.com";
+     
+    return "https://api-samgriffen-v2.herokuapp.com";
   
    }
 }
